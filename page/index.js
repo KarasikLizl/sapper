96d250dd204b613fba4c90eced13e3a51d732a31
Elizaveta Kruchinina <Kruchinina.elizaveta1@yandex.ru>
2023-03-04 12:29:17 +0300

    add timer background

--- page/index.js
+++ page/index.js
@@ -16,26 +16,40 @@ for (let i = 1; i <= 256; i++) {
 let s = '0';
 let t = '0';
 let h = '0';
+let stepUnits = '-14';
+let stepTenths = '-14';
+let stepHundr = '-14';
 
 setInterval(function () {
   s = +s + 1;
+  stepUnits = +stepUnits + 14;
+  sec.style.backgroundPosition = ` -${stepUnits}px 0`;
   if (s >= 10) {
+    stepUnits = '-28';
+    stepUnits = +stepUnits + 14;
+    stepTenths = +stepTenths + 14;
+    sec.style.backgroundPosition = ` -${stepUnits}px 0`;
+    ten.style.backgroundPosition = `-${stepTenths}px 0`;
     s = '0';
     t = +t + 1;
   }
 
   if (t >= 10) {
+    stepTenths = '-28';
+    stepTenths = +stepTenths + 14;
+    ten.style.backgroundPosition = `-${stepTenths}px 0`;
+    stepHundr = +stepHundr + 14;
+    sec.style.backgroundPosition = `-${stepUnits}px 0`;
+    ten.style.backgroundPosition = `-${stepTenths}px 0`;
+    hundr.style.backgroundPosition = `-${stepHundr}px 0`;
     s = '0';
     t = '0';
     h = +h + 1;
   }
 
   if (h >= 10) {
-    s = '9';
-    t = '9';
-    h = '9';
+    sec.style.backgroundPosition = '-112px 0';
+    ten.style.backgroundPosition = '-112px 0';
+    hundr.style.backgroundPosition = '-112px 0';
   }
-  sec.innerText = s;
-  ten.innerText = t;
-  hundr.innerText = h;
 }, 1000);
