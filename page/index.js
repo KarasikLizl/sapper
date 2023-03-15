29249a331bb09c9f328c2bf0e4e079abbf346c32
Elizaveta Kruchinina <Kruchinina.elizaveta1@yandex.ru>
2023-03-15 13:12:01 +0300

    add empty cells and timer reset

--- page/index.js
+++ page/index.js
@@ -1,6 +1,6 @@
-const sec = document.getElementById('units');
-const ten = document.getElementById('tenths');
-const hundr = document.getElementById('hundredths');
+const units = document.getElementById('units');
+const tenths = document.getElementById('tenths');
+const hundredths = document.getElementById('hundredths');
 const fieldCell = document.querySelector('.field__cell');
 const startBtn = document.querySelector('.head__btn');
 let cell = document.createElement('div');
@@ -8,18 +8,18 @@ let x = 0;
 let y = 0;
 let bombsArray = [];
 let coordinateNum = [];
-// Поле клеток
 
+// field generator
 function createField(bombs) {
   let cell = document.createElement('div');
   cell.className = 'field__cell';
   cell.setAttribute('coordinates', `${x},${y}`);
   const field = document.querySelector('.field');
+  // TODO
   cell.addEventListener('contextmenu', function (event) {
     event.preventDefault();
     clickRightBtn(event.target);
   });
-
   let cells = field.appendChild(cell);
   // coordinates
   let boardSize = 16;
@@ -29,42 +29,39 @@ function createField(bombs) {
     x++;
   }
   coordinateNum.push(`${x},${y}`);
-  document.addEventListener('click', () => {
+  document.addEventListener('click', function () {
     let coordinate = cell.getAttribute('coordinates');
     if (bombs.includes(coordinate)) {
-      cell.style.backgroundPosition = '-86px -52px';
+      cell.style.backgroundPosition = '-85px -51px';
+    } else {
+      cell.style.backgroundPosition = '-17px -51px';
     }
   });
   return cells;
 }
-
-// function b( bombs) {
-  
-// }
 // bombs generator
 for (let i = 0; i <= 40; i++) {
   bombsArray.push(`${random(0, 15)},${random(0, 15)}`);
 }
 //field
-
 for (let i = 1; i <= 256; i++) {
   createField(bombsArray);
 }
-
+// restart and new game
 const restart = () => {
   bombsArray = [];
   coordinateNum = [];
+  units.style.backgroundPosition = '-126px 0';
   x = 0;
   y = 0;
   clearInterval(timer);
-
+  timer = setInterval(tick, 1000);
   s = '0';
   t = '0';
   h = '0';
   stepUnits = '-14';
   stepTenths = '-14';
   stepHundr = '-14';
-  // setInterval(tick, 1000);
   let c = document.querySelector('.field');
   c.innerHTML = '';
   for (let i = 1; i <= 256; i++) {
@@ -73,7 +70,6 @@ const restart = () => {
   for (let i = 0; i <= 40; i++) {
     bombsArray.push(`${random(0, 15)},${random(0, 15)}`);
   }
-  
 };
 
 // timer
@@ -87,13 +83,13 @@ let timer;
 function tick() {
   s = +s + 1;
   stepUnits = +stepUnits + 14;
-  sec.style.backgroundPosition = ` -${stepUnits}px 0`;
+  units.style.backgroundPosition = ` -${stepUnits}px 0`;
   if (s >= 10) {
     stepUnits = '-28';
     stepUnits = +stepUnits + 14;
     stepTenths = +stepTenths + 14;
-    sec.style.backgroundPosition = ` -${stepUnits}px 0`;
-    ten.style.backgroundPosition = `-${stepTenths}px 0`;
+    units.style.backgroundPosition = ` -${stepUnits}px 0`;
+    tenths.style.backgroundPosition = `-${stepTenths}px 0`;
     s = '0';
     t = +t + 1;
   }
@@ -101,28 +97,28 @@ function tick() {
   if (t >= 10) {
     stepTenths = '-28';
     stepTenths = +stepTenths + 14;
-    ten.style.backgroundPosition = `-${stepTenths}px 0`;
+    tenths.style.backgroundPosition = `-${stepTenths}px 0`;
     stepHundr = +stepHundr + 14;
-    sec.style.backgroundPosition = `-${stepUnits}px 0`;
-    ten.style.backgroundPosition = `-${stepTenths}px 0`;
-    hundr.style.backgroundPosition = `-${stepHundr}px 0`;
+    units.style.backgroundPosition = `-${stepUnits}px 0`;
+    tenths.style.backgroundPosition = `-${stepTenths}px 0`;
+    hundredths.style.backgroundPosition = `-${stepHundr}px 0`;
     s = '0';
     t = '0';
     h = +h + 1;
   }
 
   if (h >= 10) {
-    sec.style.backgroundPosition = '-112px 0';
-    ten.style.backgroundPosition = '-112px 0';
-    hundr.style.backgroundPosition = '-112px 0';
+    units.style.backgroundPosition = '-112px 0';
+    tenths.style.backgroundPosition = '-112px 0';
+    hundredths.style.backgroundPosition = '-112px 0';
   }
 }
-timer = setInterval(tick, 1000);
+
 // bombs generator
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+// flag clicl
 function clickRightBtn(elem) {
   if (
     !elem.classList.contains('field__cell_flag') &&
